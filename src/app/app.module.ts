ae109666836a81a485c4e22550ed1d33a8bdf3e3
Manikandan Prabhu <manikandanrprabhu@gmail.com>
2024-07-29 11:22:37 +0530

    001: Home section component created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { MatButtonModule } from '@angular/material/button';
+import { HomeSectionComponent } from './components/home-section/home-section.component';
+import { SocialLinksComponent } from './components/social-links/social-links.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeSectionComponent, SocialLinksComponent],
+  imports: [BrowserModule, MatButtonModule, AppRoutingModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/constants/social-links.constant.ts
@@ -0,0 +1,22 @@
+import { Link } from '../model/social-links.model';
+
+export const SOCIAL_LINKS: Link[] = [
+  {
+    showLink: true,
+    image: 'instagram.svg',
+    alt: 'Instagram',
+    link: '',
+  },
+  {
+    showLink: true,
+    image: 'linkedin.svg',
+    alt: 'LinkedIn',
+    link: '',
+  },
+  {
+    showLink: true,
+    image: 'twitter.svg',
+    alt: 'Twitter',
+    link: '',
+  },
+];
