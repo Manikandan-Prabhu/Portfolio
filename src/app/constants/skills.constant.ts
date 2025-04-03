0dca14720b7eaf0ab82c0e33dc9314cedbecf03a
Manikandan Prabhu <manikandanprabhu@centizen.com>
2025-04-03 15:39:51 +0530

    Fix: Design changes

--- src/app/constants/skills.constant.ts
+++ src/app/constants/skills.constant.ts
@@ -1,9 +1,10 @@
 
 export const SKILLS = [
-  { domain: "Frontend", skills: "Angular, TypeScript, RxJS, JavaScript" },
-  { domain: "State Management", skills: "Angular, TypeScript, RxJS, JavaScript" },
-  { domain: "Testing", skills: "Angular, TypeScript, RxJS, JavaScript" },
-  { domain: "Performance Optimization", skills: "Lazy Loading, Web Components" },
+  { domain: "Frontend", skills: "Angular, TypeScript, RxJS, JavaScript, HTML, CSS" },
+  { domain: "State Management", skills: "NgRx (Signal Store)" },
+  { domain: "Testing", skills: "Jest, Jasmine" },
+  { domain: "Performance Optimization", skills: "Lazy Loading, Code Splitting, Web Components" },
+  { domain: "Tools & Version Control", skills: "Git, Sourcetree, VS Code, Postman" },
 ]
 
 
